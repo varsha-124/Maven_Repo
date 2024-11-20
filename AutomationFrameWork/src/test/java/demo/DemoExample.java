89dbae47340289081b38aae80ae48270d6accbad
Admin <Admin@DESKTOP-414SEUC>
2024-11-20 14:00:33 +0530

    New Class Added

---
+++ AutomationFrameWork/src/test/java/demo/DemoExample.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoExample {
+@Test
+public void demo()
+{
+	Reporter.log("Changes done in the project");
+}
+}
